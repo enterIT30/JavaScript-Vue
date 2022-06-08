1ccc1e54342d213f011a01c7b3656c337e0d1582
Vladyslav <ryzhykovvv@gmail.com>
2022-06-08 20:05:17 +0300

    solved tasks (boolean operators & if, else)

--- app.js
+++ app.js
@@ -106,5 +106,147 @@ obj.details = {};
 obj.details.model = 3300;
 obj.details.color = 'blue';
 
+// Boolean operators (if, else if) ==================================================================================
+
+
+// Чему равно а, почему?
+
+let $a = 0 || 'string'; // 'string'
+
+let $b = 1 && 'string'; // 'string'
+
+let $c = null || 25;  // 25
+
+let $d = null && 25;  // null
+
+let $e = null || 0 || 35; // 35
+
+let $f = null && 0 && 35; // null
+
+console.log($a, $b, $c, $d, $e, $f);
+
+// Что отобразится в консоли. Почему?
+
+console.log(12 + 14 + '12'); // '2612'
+
+console.log(3 + 2 - '1'); // 4
+
+console.log('3' + 2 - 1); // 31
+
+console.log(true + 2); // 3
+
+console.log(+'10' + 1); // 11
+
+console.log(undefined + 2); // NaN
+
+console.log(null + 5); // 5
+
+console.log(true + undefined); // Nan
+
+
+/**
+ * Создать произвольную переменную, присвоеть ей значение и написать условие,
+ * если переменная равна “hidden”, присвоить ей значение “visible”, иначе - “hidden”.
+ */
+
+let enis = 'hidden';
+
+if (enis === 'hidden') {
+  enis = 'visible';
+} else {
+  enis = 'hidden';
+}
+
+console.log(enis);
+
+
+/**
+ * Создать переменную и присвойте ей число.
+ *
+ * Используя if, записать условие:
+ *
+ * - если переменная равна нулю, присвоить ей 1;
+ * - если меньше нуля - строку “less then zero”;
+ * - если больше нуля - используя оператор “присвоение”, переменную умножить на 10 (использовать краткую запись).
+ */
+
+let numbass = 69;
+
+if (numbass == 0) {
+  numbass = 1;
+} else if (numbass < 0) {
+  numbass = 'less then zero';
+} else {
+  numbass *= 10;
+}
+
+console.log(numbass);
+
+/**
+ * Дан объект
+ * Написать условие если возраст машины больше 5 лет то нужно вывести в консоль сообщение 'Need Repair' и свойство
+ * needRepair в объекте car изменить на true; иначе изменить на false.
+ */
+
+let car = {
+  name: 'Lexus',
+  age: 10,
+  create: 2008,
+  needRepair: false,
+};
+
+if (car.age > 5) {
+  console.log('Need Repair');
+  car.needRepair = true;
+} else {
+  car.needRepair = false;
+}
+
+console.log(car);
+
+/**
+ * Дан объект
+ * Написать условие если у item есть поле discount и там есть значение которое
+ * не NaN а также есть поле price значение которого также не NaN то в объекте item
+ * создать поле priceWithDiscount и записать туда цену с учетом скидки и вывести ее
+ * в консоль, обратите внимание что поля discount и price это строки и вам из них
+ * нужно получить числа чтобы выполнить расчет. иначе если поля discount нет то
+ * вывести просто поле price в консоль.
+ */
+
+let item = {
+  name: 'Intel core i7',
+  price: '100$',
+  discount: '15%',
+};
+
+if (item.discount && isNaN(item.discount) && item.price && isNaN(item.price)) {
+  item.priceWithDiscount = `${+parseInt(item.price) - ((+parseInt(item.price) * +parseInt(item.discount)) / 100)}`;
+  console.log(item.priceWithDiscount);
+} else {
+  console.log(item.price);
+}
+
+
+
+/**
+ * Написать условие если цена товара больше или равна минимальной цене и меньше или равна максимальной цене то вывести
+ * в консоль название этого товара, иначе вывести в консоль что товаров не найдено.
+ */
+
+let product = {
+  name: 'Яблоко',
+  price: '120$',
+};
+
+let minPr = 10; // минимальная цена
+let maxPr = 20; // максимальная цена
+
+if (parseInt(product.price) >= minPr && parseInt(product.price) <= maxPr) {
+  console.log(product.name);
+} else {
+  console.log('товаров не найдено');
+}
+
 //==================================================================================
 
