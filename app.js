58fbffc9d2aa97ea04650922378476b1b389846e
Vladyslav <ryzhykovvv@gmail.com>
2022-06-08 15:36:42 +0300

    solved tasks (string)

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Number ==================================================================================
+
 // Получить число pi из Math и округлить до 2-х знаков после точки
 let p = Math.PI.toFixed(2);
 console.log(p);
@@ -32,5 +34,55 @@ console.log(sum);
 
 let number = parseInt('100$');
 console.log(number);
+
+// String ==================================================================================
+
+let string = 'some test string';
+
+// Получить первую и последнюю букву строки
+
+let firstLetter = string[0];
+let lastLetter = string[string.length - 1];
+
+console.log(firstLetter, lastLetter);
+
+// Сделать первую и последнюю буквы в верхнем регистре
+
+let toUpFirstLastLetter = `${string[0].toUpperCase()}${string.slice(1, -1)}${string[string.length - 1].toUpperCase()}`;
+
+console.log(toUpFirstLastLetter);
+
+// Найти положение слова 'string' в строке
+
+console.log(string.indexOf('string'));
+
+// Найти положение второго пробела
+let firstSpace = string.indexOf(' ');
+let secondSpace = string.indexOf(' ', firstSpace + 1);
+console.log(secondSpace);
+
+// Получить строку с 5-го символа длиной 4 буквы
+console.log(string.substr(5, 4));
+
+// Получить строку с 5-го по 9-й символ
+
+console.log(string.slice(5, 9));
+
+// Получить новую строку из исходной путем удаления последних 6-и символов
+
+let newString = string.slice(0, -6);
+console.log(newString);
+
+// Из двух переменных получить переменную string в которой будет содержаться текст 2016
+
+let a = 20;
+let b = 16;
+
+let stringNum1 = String(a) + String(b);
+let stringNum2 = `${a}${b}`;
+
+console.log(stringNum1, stringNum2);
+
+//==================================================================================
 
 
