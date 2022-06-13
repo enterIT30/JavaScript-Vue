a1517722fec73d78157482c1776d225929ef2b14
Vladyslav <ryzhykovvv@gmail.com>
2022-06-13 22:53:40 +0300

    solved tasks (array-methods)

--- app.js
+++ app.js
@@ -534,4 +534,119 @@ function guessTheNumber(num) {
 
 console.log(guessTheNumber(null));
 
+// Array methods ==================================================================================
+
+/**
+ * Создать функцию, которая принимает число т и возвращает массив, заполненный числами от 1 до n
+ */
+
+function getArray(num) {
+  let newArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    newArr[i-1] = i;
+  }
+  return newArr;
+}
+
+console.log(getArray(10));
+
+function getArray(num) {
+  let newArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    newArr.push(i);
+  }
+  return newArr;
+}
+
+console.log(getArray(10));
+
+/**
+ * Создать функцию, которая принимает массив, а возвращает новый массив с дублированными элементами
+ * сходного массива
+ */
+
+function doubleArray(arr) {
+  return arr.concat(arr);
+}
+
+console.log(doubleArray([1,2,3]));
+
+/**
+ * Создать функцию, которая принимает произвольное число массивов и удаляет из каждого массива
+ * первый элемент, а возвращает массив из оставшихся значений
+ */
+
+
+function changeCollection() {
+  let mainArr = [];
+
+  if (arguments.length) {
+    for (let i = 0; i < arguments.length; i++) {
+      if (Array.isArray(arguments[i])) {
+        arguments[i].shift();
+        mainArr.push(arguments[i]);
+      }
+    }
+  }
+  return mainArr;
+}
+
+console.log(changeCollection([1, 2, 3], ['a', 'b', 'c'], [5, 6, 7]));
+
+/**
+ * Создать функцию которая принимает массив пользоватлей, поле котрое хочу проверить и значение
+ * на которое хочу проверять указанное поле. Проверять, что все аргументы переданы. Если что-то не
+ * переданно, то возвращать объект ошибки return new Error('Error message'). Возвращать новый массив с
+ * пользователями соответствующие указанным параметрам
+ */
+
+
+const users = [
+  {
+    "_id": "5e36b779dc76fe3db02adc32",
+    "balance": "$1,955.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "name": "Berg Zimmerman",
+    "gender": "male"
+  },
+  {
+    "_id": "5e36b779d117774176f90e0b",
+    "balance": "$3,776.14",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "name": "Deann Winters",
+    "gender": "female"
+  },
+  {
+    "_id": "5e36b779daf6e455ec54cf45",
+    "balance": "$3,424.84",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "name": "Kari Waters",
+    "gender": "female"
+  }
+];
+
+
+function filterUsers(arr, key, value) {
+  let newArr = [];
+  let res;
+  if (arguments.length != 3) {
+    res = new Error('Error message');
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][key] == value) {
+        newArr.push(arr[i]);
+      }
+    }
+    res = newArr;
+  }
+  return res;
+}
+
+console.log(filterUsers(users, "age", 36));
+
 //==================================================================================
