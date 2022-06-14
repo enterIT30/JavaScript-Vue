fe880d09ed1b218f6ad61aec0bb6079c61e1e35e
Vladyslav <ryzhykovvv@gmail.com>
2022-06-15 00:25:51 +0300

    solved task (higher-order function)

--- app.js
+++ app.js
@@ -649,4 +649,57 @@ function filterUsers(arr, key, value) {
 
 console.log(filterUsers(users, "age", 36));
 
-//==================================================================================
+// Higher-Order Function ==================================================================================
+
+/**
+ * Создать функции:
+ *
+ * - первая принимает массив и колбек (одна для всех)
+ * - вторая функция колбек обрабатывает каждый элемент массива (для каждого вызова свой callback)
+ *
+ * Первая возвращает строку `New value: ` и результат обработки:
+ *
+ * firstFunc(['my', 'name', 'is', 'Trinity'], handler1)) // 'New value: MyNameIsTrynyty'
+ * firstFunc([10, 20, 30], handler2) //'New value: 100, 200, 300'
+ * firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3) // 'New value: Jhon is 45, Aaron is 20'
+ * firstFunc(['abs', '123'], handler4) // 'New value: cba, 321'
+ */
+
+// 1
+function firstFunc(arr, fn) {
+  let newArr = '';
+  for (let i = 0; i < arr.length; i++) {
+    newArr += fn(arr[i]);
+  }
+  return `New value: ${newArr.trim()}`;
+}
+
+function handler1(el) {
+  return `${el[0].toUpperCase()}${el.slice(1)}`;
+}
+
+console.log(firstFunc(['my', 'name', 'is', 'Trinity'], handler1));
+
+
+function handler2(el) {
+  return `${el * 10}, `;
+}
+
+console.log(firstFunc([10, 20, 30], handler2) )
+
+function handler3(el) {
+  return `${el.name} is ${el.age}, `;
+}
+
+console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3));
+
+function handler4(el) {
+  let newElr = '';
+  for (let i = el.length-1; i >= 0; i--) {
+    newElr += el[i];
+  }
+  return `${newElr}, `;
+}
+
+console.log(firstFunc(['abs', '123'], handler4));
+
